f5e76d307361c06d398bf0957aa7ed97559a9a31
robin901118 <303278055@qq.com>
2019-02-26 10:23:05 +0800

    添加style内引入背景图别名配置

--- vue.config.js
+++ vue.config.js
@@ -41,6 +41,13 @@ module.exports = {
       'vue-router': 'VueRouter'
     };
 
+    /*配置图片路径，在css中如果要引入背景图可以用~img即可*/
+    obj.resolve = {
+      alias: {
+        'img': '@/assets/img',
+      }
+    };
+
     /*生产环境下禁止console.log*/
     if (process.env.NODE_ENV === 'production') {
       obj.plugins = [
